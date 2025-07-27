c70e6de35426495af95f4dbd9059036878570aa6
MuhammadBurhan00 <muhammadburhann00@gmail.com>
2025-07-27 19:32:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,6 @@ def scrape_youtube_post():
         driver.quit()
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
